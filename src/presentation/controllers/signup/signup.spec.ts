3f2b0cefea6fbafe231cd1d04394f61efffe1d65
Aller Augusto de Souza <allerasouza@gmail.com>
2022-07-03 22:20:34 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -201,4 +201,33 @@ describe('SignUp Controller', () => {
             password: 'any_password'
         });
     });
+
+    test('Should return 200 if valid data is provided', () => {
+        const { sut } = makeSut();
+        // jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(true);
+        // jest.spyOn(addAccountStub, 'add').mockReturnValueOnce({
+        //     id: 'any_id',
+        //     name: 'any_name',
+        //     email: 'any_email@mail.com',
+        //     password: 'any_password'
+        // });
+        const httpRequest = {
+            body: {
+                name: 'valid_name',
+                email: 'valid_email@mail.com',
+                password: 'valid_password',
+                passwordConfirmation: 'valid_password'
+            }
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(200);
+        expect(httpResponse.body).toEqual(
+            {
+                id: 'valid_id',
+                name: 'valid_name',
+                email: 'valid_email@mail.com',
+                password: 'valid_password'
+            }
+        );
+    });
 });
